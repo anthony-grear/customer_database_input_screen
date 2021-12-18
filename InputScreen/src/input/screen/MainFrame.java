668520d811208f6d90666fdcbc89d7c6a38d0de8
Anthony Grear <tgrear2008@gmail.com>
2021-12-18 23:24:51 +0700

    Add radio button panel to frame.

--- InputScreen/src/input/screen/MainFrame.java
+++ InputScreen/src/input/screen/MainFrame.java
@@ -9,6 +9,7 @@ import java.awt.GridBagConstraints;
 
 import javax.swing.BorderFactory;
 import javax.swing.ButtonGroup;
+import javax.swing.JCheckBox;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
@@ -17,12 +18,13 @@ import javax.swing.JComboBox;
 
 public class MainFrame {
 	private JFrame mainFrame;
-	private JPanel textFieldPanel, checkBoxPanel, radioPanel, comboBoxPanel;
+	private JPanel textFieldPanel, checkBoxPanel, radioPanel, comboBoxPanel, radioPanel2;
 	private GridBagConstraints gbc;
 	private JTextField tfName, tfAge;
 	private JComboBox<String> cityCb;
 	private JRadioButton rbMale, rbFemale;
 	private ButtonGroup myGroup;
+	private JCheckBox jCb1, jCb2, jCb3, jCb4, jCb5, jCb6;
 	
 	
 	public MainFrame() {
@@ -59,6 +61,11 @@ public class MainFrame {
 		comboBoxPanel.setBackground(Color.YELLOW);
 		comboBoxPanel.setPreferredSize(new Dimension(200,100));
 		
+		radioPanel2 = new JPanel();
+		radioPanel2.setBorder(BorderFactory.createTitledBorder("JRadioButton Panel 2"));
+		radioPanel2.setBackground(Color.BLUE);
+		radioPanel2.setPreferredSize(new Dimension(200,100));
+		
 		tfName = new JTextField(10);
 		tfName.setText("Full Name");
 		tfAge = new JTextField(10);
@@ -83,6 +90,14 @@ public class MainFrame {
 		myGroup.add(rbMale);
 		myGroup.add(rbFemale);
 		
+		jCb1 = new JCheckBox("Running");
+		jCb2 = new JCheckBox("Walking");
+		jCb3 = new JCheckBox("Biking");
+		jCb4 = new JCheckBox("Swimming");
+		jCb5 = new JCheckBox("Skiing");
+		jCb6 = new JCheckBox("In-Line Skating");
+		
+		
 		
 		
 		textFieldPanel.add(tfName);
@@ -92,7 +107,12 @@ public class MainFrame {
 		radioPanel.add(rbMale);
 		radioPanel.add(rbFemale);
 		
-		
+		checkBoxPanel.add(jCb1);
+		checkBoxPanel.add(jCb2);
+		checkBoxPanel.add(jCb3);
+		checkBoxPanel.add(jCb4);
+		checkBoxPanel.add(jCb5);
+		checkBoxPanel.add(jCb6);
 		
 		
 		gbc = new GridBagConstraints();
@@ -108,6 +128,9 @@ public class MainFrame {
 		gbc.gridx=0;
 		gbc.gridy=1;
 		mainFrame.getContentPane().add(comboBoxPanel,gbc);
+		gbc.gridx=1;
+		gbc.gridy=0;
+		mainFrame.getContentPane().add(radioPanel2,gbc);
 		mainFrame.pack();
 		mainFrame.setVisible(true);
 	}
