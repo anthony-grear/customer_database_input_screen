5c9bce8379112d0606274f5c0cbbe1b77f6602fe
Anthony Grear <tgrear2008@gmail.com>
2021-12-18 22:15:37 +0700

    Add java to the nature in the .project folder. Add two textfields for Name and Age.

--- InputScreen/src/input/screen/MainFrame.java
+++ InputScreen/src/input/screen/MainFrame.java
@@ -3,17 +3,20 @@ package input.screen;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.GridBagLayout;
 import java.awt.GridBagConstraints;
 
 import javax.swing.BorderFactory;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class MainFrame {
 	private JFrame mainFrame;
 	private JPanel textFieldPanel, checkBoxPanel, radioPanel;
 	private GridBagConstraints gbc;
+	private JTextField tfName, tfAge;
 	
 	
 	public MainFrame() {
@@ -22,7 +25,6 @@ public class MainFrame {
 	
 	private void initialize() {
 		mainFrame = new JFrame();
-//		mainFrame.setLayout(new BorderLayout(10,5));
 		mainFrame.setTitle("Customer Database Input Screen");
 		mainFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		mainFrame.setSize(900,600);
@@ -34,6 +36,7 @@ public class MainFrame {
 		textFieldPanel.setBorder(BorderFactory.createTitledBorder("JTextField Panel"));
 		textFieldPanel.setBackground(Color.RED);
 		textFieldPanel.setPreferredSize(new Dimension(200,100));
+		textFieldPanel.setLayout(new FlowLayout());
 		
 		checkBoxPanel = new JPanel();
 		checkBoxPanel.setBorder(BorderFactory.createTitledBorder("JCheckBox Panel"));
@@ -44,6 +47,14 @@ public class MainFrame {
 		radioPanel.setBorder(BorderFactory.createTitledBorder("JRadioButton Panel"));
 		radioPanel.setBackground(Color.CYAN);
 		radioPanel.setPreferredSize(new Dimension(200,100));
+		
+		tfName = new JTextField(10);
+		tfName.setText("Full Name");
+		tfAge = new JTextField(10);
+		tfAge.setText("Age");
+		
+		textFieldPanel.add(tfName);
+		textFieldPanel.add(tfAge);
 		
 		gbc = new GridBagConstraints();
 		gbc.gridx=0;
